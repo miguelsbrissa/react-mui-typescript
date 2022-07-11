09926034571c662a2b56533e6038a6c1c3dfa5fc
miguelsbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-07-11 12:44:04 -0300

    feat: :sparkles: Adiciona material UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { AppRoutes } from "./routes";
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
+    
         <AppRoutes/>
-      </div>
+      
     </BrowserRouter>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import {Button} from '@mui/material';
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path='/pagina-inicial' element={<p> Página Inicial</p>}/>
-            
+            <Route path='/pagina-inicial' element={<Button>Teste</Button>}/>
+
             <Route path='*' element={<Navigate to="/pagina-inicial"/>}></Route>
         </Routes>
     );
