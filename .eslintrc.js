dc5b70dc559b6803d09da1fe80cc9c41184bddab
miguelsbrissa <84507810+miguelsbrissa@users.noreply.github.com>
2022-07-14 20:43:36 -0300

    feat: :sparkles: Cria backend e arruma modulos

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': 0,
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        "react/prop-types":"off",
+        "react/react-in-jsx-scope":"off"
+    }
+}
---
+++ mock/README.md
@@ -0,0 +1,11 @@
+#   Como usar
+1. Abra uma nova aba do terminal 
+2. Execute o seguint comando:
+
+   ```
+   npm run json-server --watch -p 3333 ./mock/database.json
+   ```
+   ou
+   ```
+   yarn run json-server --watch -p 3333 ./mock/database.json
+   ```
